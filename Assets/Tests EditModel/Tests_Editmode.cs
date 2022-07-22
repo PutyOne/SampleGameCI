5531fba972329e87d18071b5f65a64e6f650e20e
Legion5Pro-Puty <l.putala@gmail.com>
2022-07-22 11:12:19 +0200

    update tests

--- Assets/Tests EditModel/Tests_Editmode.cs
+++ Assets/Tests EditModel/Tests_Editmode.cs
@@ -11,6 +11,13 @@ public class NewTestScript
     public void NewTestScriptSimplePasses()
     {
         // Use the Assert class to test conditions
+
+        var t = 10;
+        var r = 2;
+
+        var resultOfMultiply = 20;
+
+        Assert.IsTrue(t * r == resultOfMultiply);
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
@@ -18,8 +25,13 @@ public class NewTestScript
     [UnityTest]
     public IEnumerator NewTestScriptWithEnumeratorPasses()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
+        var go = new GameObject();
+        go.AddComponent<Rigidbody>();
+        var originalPosition = go.transform.position.y;
+
         yield return null;
+
+        Assert.IsTrue(true);
     }
+
 }
---
+++ Assets/Tests PlayMode/Tests_Playmode.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        // Use the Assert class to test conditions
+
+        var t = 10;
+        var r = 2;
+
+        var resultOfMultiply = 20;
+
+        Assert.IsTrue(t * r == resultOfMultiply);
+
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        var go = new GameObject();
+        go.AddComponent<Rigidbody>();
+        var originalPosition = go.transform.position.y;
+
+        yield return new WaitForFixedUpdate();
+
+        Assert.AreNotEqual(originalPosition, go.transform.position.y);
+    }
+
+
+    
+}
